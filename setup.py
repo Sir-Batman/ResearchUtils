dba0f46cb0c0d18cf4b5580c926f8599f5190e4b
Connor Yates <yatesco@oregonstate.edu>
2022-02-16 16:17:21 -0800

    Add install_requires to setup.py, improve packaging

--- setup.py
+++ setup.py
@@ -14,4 +14,9 @@ setuptools.setup(
     url="https://github.com",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+    ]
 )
